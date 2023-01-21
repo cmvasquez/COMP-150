dfd2fd1ec134138ca6bfd645802942e09ec5c349
cmvasquez <cmvasquez2003@gmail.com>
2023-01-21 17:44:18 -0600

    Committing Lab 1 - BMI.py

---
+++ Lab 1 - BMI/BMI.py
@@ -0,0 +1,10 @@
+# BMI
+# This program calculates the BMI of a person
+# given their height and weight.
+# Written by Christian Vasquez COMP 150 1/21/23
+
+name = "Jose"
+weight = 60
+height = 55
+bmi = weight / height**2 * 703
+print(name + "'s BMI is " + str(bmi))
