64c180af50ae77429dcc1427ce2882727b2146ee
cmvasquez <cmvasquez2003@gmail.com>
2023-01-21 17:55:08 -0600

    Committing Lab 2 - Welcome.py

---
+++ Lab 2 - Welcome/Welcome.py
@@ -0,0 +1,12 @@
+# Welcome
+# This program takes user input for their first and last names,
+# and it prints a welcome statement using their full name.
+# Written by Christian Vasquez COMP 150 1/21/23
+
+
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+greetingFoo = "Hello there"
+greetingBar = ", it is nice to meet you! I am simply a program, so by the time" \
+              " \nyou are reading this I will have ceased to exist."
+print(greetingFoo, firstName, lastName, greetingBar)
