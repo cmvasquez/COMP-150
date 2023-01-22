d67a9ad7abe6c5ebf505160f58ba6347fe865054
cmvasquez <cmvasquez2003@gmail.com>
2023-01-21 18:11:16 -0600

    Committing Lab 3 - add3.py

---
+++ Lab 3 - Add3/add3.py
@@ -0,0 +1,11 @@
+# 1.10.3 add3.py (p 27).
+# This program asks for three numbers, and lists
+# all three, and their sum
+# Written by Christian Vasquez COMP 150 1/21/23
+
+x = int((input("Enter an integer: ")))
+y = int((input("Enter a 2nd integer: ")))
+z = int((input("Enter a final integer: ")))
+sum = x+y+z
+print('The sum of ' + str(x) + ', ' + str(y) +
+      ', and ' + str(z) + ' is ' + str(sum) + '.')
