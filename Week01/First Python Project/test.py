8d2c625e53f5443903092417a6292181cceb4d8d
cmvasquez <cmvasquez2003@gmail.com>
2023-01-24 10:53:53 -0600

    committing fixed folders for week01 and week02

---
+++ Week01/First Python Project/test.py
@@ -0,0 +1,9 @@
+animalOne = input("Enter an animal: ")                                                      # Prompts for animal
+animalTwo = input("Enter a second animal: ")                                                # Prompts for second animal
+noun = input("Enter a noun (object): ")                                                     # Prompts for object
+actionVerb = input("Enter an action verb: ")                                                # Prompts for action verb
+print("I was at the zoo yesterday. I really enjoy looking at the animals"                   # Print mad lib
+      ", \nespecially the " + animalOne +
+      "s. I also got to see the " + animalTwo + "s,"
+      " but \nthey didnt seem to like me, one of them was " +
+      actionVerb + " all \nover the place, and another one threw a " + noun + " at me!")
