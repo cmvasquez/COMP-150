4feb31f168f291786f7537d5b941a6c60a65a4e5
cmvasquez <cmvasquez2003@gmail.com>
2023-02-14 10:24:06 -0600

    Submitting Week05 Lab 2

--- Week05/Lab 2 - UserID/vasquezUserID.py
+++ Week05/Lab 2 - UserID/vasquezUserID.py
@@ -1,10 +1,28 @@
-'''Write a function that prompts the user for the first and
-last name and returns both to the main function. Write another
-function that takes first and last name as parameters, extracts
-the first (1) character of the first name and first 6 characters
-of the last name, or the entire length if less than 6, to create
-and return the userId – all in lowercase regardless of input.
-Write a main function to invoke the functions above and print
-out the first and last name and the userId (in lowercase).
-Your program will work for any string length.
+''' Christian Vasquez | 2/14/23 | COMP 150
+    Lab 2 - User ID
+
+    This program takes user input for a first and last
+    name, and then creates a user id using the first
+    letter of the first name and the first 6 (or all if
+    less) letters of the last name, all in lowercase.
 '''
+
+def nameInput():                                            # Function to take user input
+    firstName = input("Please enter your first name: ")     # Input first name
+    lastName = input("Please enter your last name: ")       # Input last name
+    return firstName, lastName                              # Return both
+
+
+def createID(firstName, lastName):                          # Method to create user ID using first and last
+    userID = firstName[0].lower() + lastName[:6].lower()    # Concatenate user ID
+    return userID                                           # Return user ID
+
+
+def main():                                                 # Main function to invoke other functions
+    firstName, lastName = nameInput()                       # Set firstName and lastName locally in main
+    userID = createID(firstName, lastName)                  # Get userID from firstName and lastName
+    print(f"Name: {firstName} {lastName}")                  # Print original name
+    print(f"User ID: {userID}")                             # Print user ID
+
+main()                                                      # Run the program
+
