3f7e25a1a33afc1f8f883be6a12523bc89e44a3d
cmvasquez <cmvasquez2003@gmail.com>
2023-04-02 17:03:02 -0500

    committing Week10

--- Week10/Programming Project 2 - Rock Paper Scissors/vasquezRockPaperScissors.py
+++ Week10/Programming Project 2 - Rock Paper Scissors/vasquezRockPaperScissors.py
@@ -1,7 +1,11 @@
 ''' Christian Vasquez | COMP 150 | 4/2/23
     Programming Project 2 - vasquezRockPaperScissors.py
 
-
+    This program allows the user to play rock paper scissors
+    against a computer opponent making use of random choices.
+    It keeps track of score, and allows the user to quit at any
+    time by pressing the quit button. The game is played in a
+    graphic window.
 '''
 from graphics import *
 import random
@@ -10,137 +14,137 @@ import random
 win = GraphWin("Rock-Paper-Scissors", 400, 300)
 win.setCoords(0, 0, 4, 3)
 
-# Draw buttons
-rock_button = Rectangle(Point(0.5, 0.5), Point(1.5, 1.5))
-rock_button.setFill("gray")
-rock_button.draw(win)
-rock_label = Text(Point(1, 1), "ROCK")
-rock_label.draw(win)
+# Draw rock button
+rockButton = Rectangle(Point(0.5, 0.5), Point(1.5, 1.5))
+rockButton.setFill("gray")
+rockButton.draw(win)
+rockLabel = Text(Point(1, 1), "ROCK")
+rockLabel.draw(win)
 
-paper_button = Rectangle(Point(1.5, 0.5), Point(2.5, 1.5))
-paper_button.setFill("blue")
-paper_button.draw(win)
-paper_label = Text(Point(2, 1), "PAPER")
-paper_label.draw(win)
+# Draw paper button
+paperButton = Rectangle(Point(1.5, 0.5), Point(2.5, 1.5))
+paperButton.setFill("blue")
+paperButton.draw(win)
+paperLabel = Text(Point(2, 1), "PAPER")
+paperLabel.draw(win)
 
-scissors_button = Rectangle(Point(2.5, 0.5), Point(3.5, 1.5))
-scissors_button.setFill("orange")
-scissors_button.draw(win)
-scissors_label = Text(Point(3, 1), "SCISSORS")
-scissors_label.draw(win)
+# Draw scissors button
+scissorsButton = Rectangle(Point(2.5, 0.5), Point(3.5, 1.5))
+scissorsButton.setFill("orange")
+scissorsButton.draw(win)
+scissorsLabel = Text(Point(3, 1), "SCISSORS")
+scissorsLabel.draw(win)
 
-quit_button = Rectangle(Point(1.5, 2), Point(2.5, 2.5))
-quit_button.setFill("red")
-quit_button.draw(win)
-quit_label = Text(Point(2, 2.25), "QUIT")
-quit_label.setTextColor("white")
-quit_label.draw(win)
+# Draw quit button
+quitButton = Rectangle(Point(1.5, 2), Point(2.5, 2.5))
+quitButton.setFill("red")
+quitButton.draw(win)
+quitLabel = Text(Point(2, 2.25), "QUIT")
+quitLabel.setTextColor("white")
+quitLabel.draw(win)
 
 # Define score variables
-player_score = 0
-computer_score = 0
+playerScore = 0
+computerScore = 0
 
-# Define function to get player choice
-def get_player_choice():
-    while True:
-        click_point = win.getMouse()
-        if click_point.getX() >= 0.5 and click_point.getX() <= 1.5 and click_point.getY() >= 0.5 and click_point.getY() <= 1.5:
+
+def getPlayerChoice():  # Function to get player's choice
+    while True:  # While the player has not made a choice
+        clickPoint = win.getMouse()  # Get the point the mouse clicks on
+        if 0.5 <= clickPoint.getX() <= 1.5 and 0.5 <= clickPoint.getY() <= 1.5:  # Rock button
             return "ROCK"
-        elif click_point.getX() >= 1.5 and click_point.getX() <= 2.5 and click_point.getY() >= 0.5 and click_point.getY() <= 1.5:
+        elif 2.5 >= clickPoint.getX() >= 1.5 >= clickPoint.getY() >= 0.5:  # Paper button
             return "PAPER"
-        elif click_point.getX() >= 2.5 and click_point.getX() <= 3.5 and click_point.getY() >= 0.5 and click_point.getY() <= 1.5:
+        elif 2.5 <= clickPoint.getX() <= 3.5 and 0.5 <= clickPoint.getY() <= 1.5:  # Scissors button
             return "SCISSORS"
-        elif click_point.getX() >= 1.5 and click_point.getX() <= 2.5 and click_point.getY() >= 2 and click_point.getY() <= 2.5:
+        elif 1.5 <= clickPoint.getX() <= 2.5 and 2 <= clickPoint.getY() <= 2.5:  # Quit button
             return "quit"
-            break
 
-# Define function to get computer choice
-def get_computer_choice():
-    choices = ["ROCK", "PAPER", "SCISSORS"]
-    return random.choice(choices)
 
-# Define function to determine winner
-def determine_winner(player_choice, computer_choice):
-    global player_score, computer_score
+def getComputerChoice():  # Function to get computer choice
+    choices = ["ROCK", "PAPER", "SCISSORS"]  # Either rock, paper, or scissors
+    return random.choice(choices)  # Pick a random one, return
 
-    if player_choice == computer_choice:
-        return "tie"
-    elif (player_choice == "ROCK" and computer_choice == "SCISSORS") or (
-            player_choice == "PAPER" and computer_choice == "ROCK") or (
-            player_choice == "SCISSORS" and computer_choice == "PAPER"):
-        player_score += 1
-        return "player"
+
+def determineWinner(playerChoice, computerChoice):  # Function to determine the winner
+    global playerScore, computerScore  # Global variables to access across program
+
+    if playerChoice == computerChoice:  # If both player and cpu choose same
+        return "tie"  # It's a tie
+    elif (playerChoice == "ROCK" and computerChoice == "SCISSORS") or (  # If player beats computer
+            playerChoice == "PAPER" and computerChoice == "ROCK") or (
+            playerChoice == "SCISSORS" and computerChoice == "PAPER"):
+        playerScore += 1  # Increase player score
+        return "player"  # Player wins
+    else:  # If not,
+        computerScore += 1  # Increase computer score
+        return "computer"  # Computer wins
+
+
+def updateScoreDisplay():  # Function to update scoreboard
+    global playerScoreLabel, computerScoreLabel  # Global variables to access across program
+
+    if playerScoreLabel:
+        playerScoreLabel.setText("Player: " + str(playerScore))
     else:
-        computer_score += 1
-        return "computer"
+        playerScoreLabel = Text(Point(1.5, 2.75), "Player: " + str(playerScore))
+        playerScoreLabel.draw(win)
 
-def update_score_display():
-    global player_score_label, computer_score_label
-
-    if player_score_label:
-        player_score_label.setText("Player: " + str(player_score))
+    if computerScoreLabel:
+        computerScoreLabel.setText("Computer: " + str(computerScore))
     else:
-        player_score_label = Text(Point(1.5, 2.75), "Player: " + str(player_score))
-        player_score_label.draw(win)
+        computerScoreLabel = Text(Point(2.5, 2.75), "Computer: " + str(computerScore))
+        computerScoreLabel.draw(win)
 
-    if computer_score_label:
-        computer_score_label.setText("Computer: " + str(computer_score))
-    else:
-        computer_score_label = Text(Point(2.5, 2.75), "Computer: " + str(computer_score))
-        computer_score_label.draw(win)
 
-def main():
-    global player_score, computer_score, player_score_label, computer_score_label
+def main():  # Main function to run the program
+    global playerScore, computerScore, playerScoreLabel, computerScoreLabel  # Global variables to access across pgm
 
     # Draw initial score display
-    player_score_label = Text(Point(1.5, 2.75), "Player: " + str(player_score))
-    player_score_label.draw(win)
-    computer_score_label = Text(Point(2.5, 2.75), "Computer: " + str(computer_score))
-    computer_score_label.draw(win)
-
-    player_choice = ""
+    playerScoreLabel = Text(Point(1.5, 2.75), "Player: " + str(playerScore))
+    playerScoreLabel.draw(win)
+    computerScoreLabel = Text(Point(2.5, 2.75), "Computer: " + str(computerScore))
+    computerScoreLabel.draw(win)
 
     while True:
-        # Get player choice
-        player_choice = get_player_choice()
+        playerChoice = getPlayerChoice()  # Get player choice
 
-        # Get computer choice
-        computer_choice = get_computer_choice()
+        computerChoice = getComputerChoice()  # Get computer choice
 
-        # Determine winner
-        winner = determine_winner(player_choice, computer_choice)
+        winner = determineWinner(playerChoice, computerChoice)  # Determine winner
 
-        # Update score display
-        update_score_display()
+        updateScoreDisplay()  # Update score display
 
         # Display choices and winner
-        player_choice_label = Text(Point(.75, 2), "You: " + (player_choice))
-        player_choice_label.draw(win)
+        playerChoiceLabel = Text(Point(.75, 2), "You: " + (playerChoice))
+        playerChoiceLabel.draw(win)
+        computerChoiceLabel = Text(Point(3.25, 2), "CPU: " + computerChoice)
+        computerChoiceLabel.draw(win)
 
-        computer_choice_label = Text(Point(3.25, 2), "CPU: " + computer_choice)
-        computer_choice_label.draw(win)
-
-        if player_choice == "quit":
-            break
-        elif winner == "tie":
-            result_label = Text(Point(2, 1.8), "It's a tie!")
-        elif winner == "player":
-            result_label = Text(Point(2, 1.8), "You win!")
-        else:
-            result_label = Text(Point(2, 1.8), "Computer wins!")
-        result_label.draw(win)
+        if playerChoice == "quit":      # If player clicked quit
+            break                       # Close program
+        elif winner == "tie":           # If it's a tie
+            resultLabel = Text(Point(2, 1.8), "It's a tie!")
+        elif winner == "player":        # If player wins
+            resultLabel = Text(Point(2, 1.8), "You win!")
+        else:                           # If computer wins
+            resultLabel = Text(Point(2, 1.8), "Computer wins!")
+        resultLabel.draw(win)           # Draw the result label
 
         # Wait for click before continuing
-        click_point = win.getMouse()
+        nextGameLabel = Text(Point(2, .25), "Click anywhere to play again")
+        nextGameLabel.draw(win)
+        clickPoint = win.getMouse()
+        nextGameLabel.undraw()
 
         # Clear labels and score display
-        player_choice_label.undraw()
-        computer_choice_label.undraw()
-        result_label.undraw()
+        playerChoiceLabel.undraw()
+        computerChoiceLabel.undraw()
+        resultLabel.undraw()
 
-    player_score_label.undraw()
-    computer_score_label.undraw()
+    playerScoreLabel.undraw()
+    computerScoreLabel.undraw()
     win.close()
 
 
-main()
+main()  # Run the program
